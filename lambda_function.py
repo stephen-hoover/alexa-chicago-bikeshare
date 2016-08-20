7c78813475a096892d050fe15446cfa50d6ba8db
Stephen Hoover <shoover@civisanalytics.com>
2016-08-20 08:58:51 -0500

    REF Use config module, not file

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,9 @@
 from __future__ import print_function, division
 
+from divvy import config
 from divvy import location
 from divvy import handle
 from divvy import reply
-
-import yaml
 
 
 # --------------------------- Lambda Function ----------------------------------
@@ -15,20 +14,14 @@ def lambda_handler(event, context):
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
-    conf = get_conf()
     # This `if` prevents other Skills from using this Lambda
-    if event['session']['application']['applicationId'] != conf['app']['APP_ID']:
+    if event['session']['application']['applicationId'] != config.APP_ID:
         raise ValueError("Invalid Application ID")
 
-    st_list = location.get_stations(conf['app']['divvy_api'])
+    st_list = location.get_stations(config.divvy_api)
 
     if event['request']['type'] == "IntentRequest":
         return handle.intent(event['request'], event['session'], st_list)
     else:
         # This could be a "LaunchRequest"
-        return reply.build("<speak>Ask me a question about a Divvy station.</speak>")
-
-
-def get_conf():
-    with open('config.yaml', 'r') as _fin:
-        return yaml.load(_fin)
+        return reply.build("Ask me a question about a Divvy station.")
